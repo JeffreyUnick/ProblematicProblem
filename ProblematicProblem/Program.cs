e61f8ef531dc058167aba03cbe0a2101353a442d
UNICK J <jeffreyodeloui@gmail.com>
2025-11-14 23:22:58 -0500

    version corrected

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -10,7 +10,7 @@ namespace ProblematicProblem
        static bool cont = true;
 
        internal static List<string> activities = new List<string>()
-    { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
+    { "Movies", "Paintball", "Bowling", "Laser Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
         static void Main(string[] args)
         {
             Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
@@ -75,7 +75,7 @@ namespace ProblematicProblem
         Console.WriteLine();
                 int randomNumber = rng.Next(activities.Count);
                 string randomActivity = activities[randomNumber];
-                if (userAge < 21 && randomActivity == "Wine Tasting")
+        if (userAge < 21 && randomActivity == "Wine Tasting")
         {
             Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
             Console.WriteLine("Pick something else!");
